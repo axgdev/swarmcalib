20996455668f164acac22de88f67432e4e20e17e
Axel Garcia <axgdev@gmail.com>
2016-07-07 17:06:20 +0200

    Removed some unused code, improved formatting

--- calibrationV2.py
+++ calibrationV2.py
@@ -3,35 +3,21 @@
 #
 #  calibration.py
 #  
-#  Copyright 2016  <>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
 import finkenPID
 import time
-import json
+#import json  #Remove unused module
 import datetime
+#To save the calibration parameters
 import calibrationOutput
+#from os import walk #Remove unused module
+
+#ROS libraries... maybe the communication python files should have it
 import rospy
-from os import walk
-import Ivy_Calibration_Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose2D
+#Ivy cal node
+import ivyModules.Ivy_Calibration_Node
 
 class Calibrator:
     """Class to calibrate the copter"""
@@ -124,7 +110,6 @@ myCalibrator.setBasePosition(0,0)
 myCalibrator.setPollingTime(0.5)
 myCalibrator.setAircraftID(5)
 Ivy_Calibration_Node.IvyInitStart()
-time.sleep(5)
 while(True):
     myCalibrator.getXYCoordinates()
     if (myCalibrator.isInDeadZone()):
