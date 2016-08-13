e0460ae9d362b249cc4d727a36e3bbd2a5435a08
Axel Garcia <axgdev@gmail.com>
2016-08-13 23:42:15 +0200

    Made indentation consistent. Deleted unneeded comments, deleted unneeded methods. Deleted redundant call for killing the copter. Deleted redundant call to set the calibration parameters. Added a small manual of how to start everything, and also the moved the info from README to the developer journal to remember some important stuff in our meetings. Maybe useful too for later documentation.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# swarmcalib
+#swarmcalib
 Swarm Calibration project
 
 Project in python which has 3 parts:
@@ -6,27 +6,68 @@ Project in python which has 3 parts:
 -Communication to V-REP
 -Calibration
 
+#Paparazzi, Ground station side: 
+###(Parallel task to Camera side)
+1.  Go into the git "calib" branch of paparazzi:
+     Changes of the paparazzi branch include 
+        - Killing copter through Ivy without remote control overriding it.
+        - Turning front copter leds to green and back to red.
+        - Control of copter pitch, roll, yaw remotely. This is added to
+          remote control, mening control with both is possible.
+2.  Go to paparazzi folder, type in console: su sh conf/system/ftdi.sh 
+    This needs a password. Get the password from someone in swarmlab.
+3.  Run paparazzi, type in console: ./paparazzi
+4.  Note the aircraftID in the upper left side of the paparazzi window,
+    this is an important input for the calibration python script.
+5.  Click on build
+6.  Put battery in copter, use gray cable to connect to copter, then 
+    click on upload to flash the copter with the software.
+7.  After copter is flashed succesfully, take battery off from copter.
+8.  Connect ground station to copter, wait for one light to be on.
+9.  Connect the battery to the copter, now the ground station should show
+    two lights on (if they blink its also ok)
+10. Click on "execute" on paparazzi window upper right hand side, this
+    will bring up the GCS window, which shows the state of the copter
+    and several other parameters. 
+    For calibration the states are important, such as: Empty, Start, 
+    inAir and so on. You can see the calibration parameters in: 
+    settings -> body2imu. These parameters will be automatically 
+    modified by the calibration script.
 
-20160812-Comments:
+#Camera side: 
+###(Parallel task to Paparazzi)
+Please note ROS needs to be installed, in the swarmlab desktop it is
+already installed.
+#(Note to self: CHECK THIS)
 
-We can set internal and external zones with check values to see how the copter position has changed.
+1.  You can either go to the calibration user in the computer or download
+    clone the git repository "tracking"
+2.  Run in console: roslaunch swarmlab_tracking cam.launch
+3.  Run in console: roslaunch swarmlab_tracking track.launch
+4.  Run in console: roslaunch swarmlab_tracking viz.launch
 
-What do the calibration parameters look like in the arena: 
-___________________________
-|          +theta          |
-|           |              |
-|   +phi____|_____-phi     |
-|           |              |
-|           |              |
-|   Green copter part      |
-____________________________
-      THE OBSERVER
-      
-Calibration parameters that worked good:
-0.284
-0.305
+Camera should be running at this point.
 
-Good PID values:
-P=0.175
-I=0.000001
-D=0
+#Calibration script side: 
+###(After Camera and Paparazzi running)
+Please note ROS needs to be installed. In the finken laptop it is
+installed under the catkin_workspace folder in the home folder. The
+instruction step 1 applies to the laptop.
+
+1.  Run in console: source $HOME/catkin_workspace/devel/setup.bash
+2.  Run in console: 
+ROS_MASTER_URI="http://finken-brain:11311" python "$HOME/calib/swarmcalib/calibrationV2.py"
+
+That's it! If the camera is working correctly, the calibration should
+be getting data, and trying to correct the copter position and finding
+calibration parameters
+
+##Some other comments and observations during our work:
+An additional dummy parameter was necessary to be added to the direction
+messages for a very strange and obscure error that occurs when all the
+parameters are float, I think the first one has to be an integer 
+#(Note to self: CHECK THIS)
+
+Sometimes the copter initial flight does not work. For these cases, make
+sure that before you start the calibration script in the GCS the state
+is set to "Empty".
--- calibrationV2.py
+++ calibrationV2.py
@@ -10,7 +10,6 @@ import time
 import datetime
 #To save the calibration parameters
 import calibrationOutput
-#from os import walk #Remove unused module
 
 #ROS libraries... maybe the communication python files should have it
 import rospy
@@ -22,9 +21,12 @@ import math
 import numpy
 
 class Calibrator:
-    """Class to calibrate the copter"""
-
+    """ Class to calibrate the copter, before starting it, it is 
+        important to set aircraftID, deadZone, basePosition (setPoint), 
+        and PollingTime
     
+    """
+
     def __init__(self):
     #Here we can put some default variables for deadzone, targetzone and pollingTime and PID parameters
         self.targetXController = finkenPID.PIDController(0.0175, 0.0000001, 0) #I set it to zero here for zero control
@@ -37,23 +39,47 @@ class Calibrator:
         self.airBlockInteger = 2
         self.emptyBlockInteger = 1
         self.landingBlockInteger = 4
-	self.internalZoneSize = 50.0
-	self.externalZoneSize = 75.0
-	self.inInternalZone = False
-	self.inExternalZone = True
-	self.accumulateX = 0.0
-	self.accumulateY = 0.0
+        self.internalZoneSize = 50.0
+        self.externalZoneSize = 75.0
+        self.inInternalZone = False
+        self.inExternalZone = True
+        self.accumulateX = 0.0
+        self.accumulateY = 0.0
         self.accumulateIter = 0
-	self.accumulateXAvg = 0
-	self.accumulateYAvg = 0
+        self.accumulateXAvg = 0
+        self.accumulateYAvg = 0
     
     #Important INIT
     def setBasePosition(self, posX, posY):
+        """ PID Setpoint. Where the copter should be. In theory this point
+            could be moved every certain time and make the copter follow
+            a pattern, however here in this calibration point we just set
+            one point and keep it throughout all the calibration routine
+            
+            Parameters
+            ----------
+            posX: float value
+                The X position we want the copter to be in
+            posY: float value
+                The Y position we want the copter to be in
+        
+        """
         self.baseX = posX
         self.baseY = posY
     
     #Important INIT
     def setDeadZone(self, minX, maxX, minY, maxY):
+        """ This defines the area where the copter should be killed. It
+            should be killed either because the copter is unstable (close
+            to walls) or because the camera detection is unreliable.
+            
+            Parameters
+            ----------
+            minX,maxX,minY,maxY: float value
+                These represent a rectangle. Outside of this rectangle is
+                the deadZone. We do a check with the function isInDeadZone
+                
+        """
         self.minX = minX
         self.maxX = maxX
         self.minY = minY
@@ -61,25 +87,50 @@ class Calibrator:
 
     #Important INIT
     def setPollingTime(self, pollingTime):
+        """ For the PID parameters we need to keep track of the changes
+            of the error throughout the time. We set here that discrete
+            time of the PID loop.
+            
+            Parameters
+            ----------
+            pollingTime: float value
+                Time in seconds. Example: pollingTime=1 is 1000ms. 
+                pollingTime=0.1 is 100ms
+        
+        """
         self.pollingTime = pollingTime
         
     #Important INIT
     def setAircraftID(self, aircraftID):
+        """ Every time we flash the copter in Paparazzi, we set an
+            aircraftID, this is important to send the messages through
+            Ivy as we need this aircraftID
+            
+            Parameters
+            ----------
+            aircraftID: integer value
+        
+        """
         self.aircraftID = aircraftID
         
     def getXYCoordinates(self):
-         #Call here the ivy method, it should return XY coordinates
-         #We will set here 
-         #self.copterXPos
-         #self.copterYPos
-         myObj = self.myIvyCalNode.IvyGetPos()
-         self.copterXPos = myObj.x
-         self.copterYPos = myObj.y
-         self.copterTheta = myObj.theta
-         print("X: "+str(self.copterXPos) + " Y: "+str(self.copterYPos) + " Theta: " + str(self.copterTheta))
+        """ Calls the ivy node and the ivy node calls ROS to get the 
+            position values from the tracking software, then we save them 
+            in the class variables copterXPos, copterYPos and copterTheta
+        
+        """
+        myObj = self.myIvyCalNode.IvyGetPos()
+        self.copterXPos = myObj.x
+        self.copterYPos = myObj.y
+        self.copterTheta = myObj.theta
+        print("X: "+str(self.copterXPos) + " Y: "+str(self.copterYPos) + " Theta: " + str(self.copterTheta))
          
     def killCopter(self):
-        #Call kill copter command
+        """ Turns off the copter motors immediately through an Ivy message.
+            We then wait a small amount of time and send the switch block
+            empty to get the copter ready for the next flight.
+        
+        """
         print("Copter Kill signal")
         self.myIvyCalNode.IvySendKill(self.aircraftID)
         time.sleep(0.1)
@@ -87,98 +138,117 @@ class Calibrator:
         return
         
     def unkillCopter(self):
+        """ Gets the copter out of the KILL status
+        
+        """
         print("Unkilling Copter")
         self.myIvyCalNode.IvySendUnKill(self.aircraftID)
         return
         
     def sendStartMode(self):
+        """ First it sets the empty block and then we put the copter in
+            air mode to be able to fly the copter giving thrust with
+            the remote control
+        
+        """
         print("Sending start mode")
         self.myIvyCalNode.IvySendSwitchBlock(self.aircraftID,self.emptyBlockInteger)
         time.sleep(0.1)
         self.myIvyCalNode.IvySendSwitchBlock(self.aircraftID,self.airBlockInteger)
         
-    def sendPitch(self, pitchToSend):
-        #Call send pitch
-        return
-        
-    def sendRoll(self, rollToSend):
-        #Call send roll
-        return
-        
     def sendParametersToCopter(self, pitchToSend, rollToSend, yawToSend):
-        #IvyCalibrationNode.IvySendParams
+        """ Parameters to control the pitch, roll and yaw of the copter.
+            It is used by the calibration to bring back the copter to
+            the set point. In Paparazzi, these values are added to the
+            remote control input, meaning that we can control the copter
+            using the copter, and we could use the remote if necessary to
+            avoid some heavy crash of the copter or something similar.
+            
+            Parameters
+            ----------
+            pitch, roll, yaw: float values
+                Use this values to make the copter move in a certain
+                direction
+        
+        """
         print("roll: "+str(rollToSend)+" pitch: "+str(pitchToSend))
-	print()
+        print() #A new line for nicer output.
         self.myIvyCalNode.IvySendCalParams(self.aircraftID, 0, rollToSend, pitchToSend, yawToSend)
         return
         
-    def sendYaw(self):
-        #Call send yaw, I think this one is not required.
-        return
+
         
     def followTarget(self):
+        """ One of the main functions of the calibration. Once we have
+            the position of the copter and setPoint (basePosition) we
+            calculate the error which is the difference between the
+            copter position and the setPoint, then we proceed to calculate
+            the new parameters we have to send to the copter making a PID
+            step.
+        
+        """
+        """ Calculate error as difference from copter pos and setPoint pos """
         errorX = (self.copterXPos - self.baseX) 
         errorY = (self.copterYPos - self.baseY)
-        
-	
-	
-		
-	
 
-	
+        """ Do axis transformation using transformation matrix 
+            from what the camera sees to what the copter sees """
         coord = numpy.array([errorX,errorY])
         self.copterTheta=self.copterTheta*math.pi/180
         translationMatrix = numpy.matrix([[math.cos(self.copterTheta), math.sin(self.copterTheta)], [-math.sin(self.copterTheta), math.cos(self.copterTheta)]])
         newCoord = numpy.dot(translationMatrix,coord)
-        #print(newCoord);
+        """ Overwrite the old errors with transformed errors """
         errorX = newCoord.item(0)
         errorY = newCoord.item(1)
 
-	"""Error Accumulation if in safe zone"""
+        """Error Accumulation if in safe zone"""
         if (math.fabs(errorX) < self.internalZoneSize) and (math.fabs(errorY) < self.internalZoneSize):
-	    print("entering internal")
-	    self.inInternalZone = True
+        print("entering internal")
+        self.inInternalZone = True
             self.accumulateX = self.accumulateX + errorX
-	    self.accumulateY = self.accumulateY + errorY
-	    self.accumulateIter = self.accumulateIter + 1
+        self.accumulateY = self.accumulateY + errorY
+        self.accumulateIter = self.accumulateIter + 1
         elif (self.inInternalZone == True):
- 	    print("exiting internal Zone")
+        print("exiting internal Zone")
             self.accumulateXAvg =(self.accumulateX/self.accumulateIter)*self.targetXController.p/10
-	    self.accumulateYAvg =(self.accumulateY/self.accumulateIter)*self.targetYController.p/10
-	    print('accuX' + str(self.accumulateX) + ' accuIter:' +str(self.accumulateIter))
-	    self.inInternalZone = False
-	    #set Pitch calib
+        self.accumulateYAvg =(self.accumulateY/self.accumulateIter)*self.targetYController.p/10
+        print('accuX' + str(self.accumulateX) + ' accuIter:' +str(self.accumulateIter))
+        self.inInternalZone = False
+        #set Pitch calib
             print("setting pitch")
             self.myIvyCalNode.IvySendCalib(self.aircraftID, 59, self.accumulateXAvg)
-	    #set Roll Calib
-	    self.myIvyCalNode.IvySendCalib(self.aircraftID, 58, -self.accumulateYAvg)
-            		
-
-	print('ErrorX: '+str(errorX)+' ErrorY: '+str(errorY))
-
-        if (math.fabs(errorX) <= self.internalZoneSize):
-            errorX = 0
-	    
-        if (math.fabs(errorY) <= self.internalZoneSize):
-            errorY = 0
-
-
-
+        #set Roll Calib
+        self.myIvyCalNode.IvySendCalib(self.aircraftID, 58, -self.accumulateYAvg)
 
         print('ErrorX: '+str(errorX)+' ErrorY: '+str(errorY))
 
+        if (math.fabs(errorX) <= self.internalZoneSize):
+            errorX = 0
+        if (math.fabs(errorY) <= self.internalZoneSize):
+            errorY = 0
+
+        print('ErrorX: '+str(errorX)+' ErrorY: '+str(errorY))
+
+        """ Get output from the controllers based on the error we have """
         rollToSend = self.targetXController.step(errorY, self.pollingTime)
         pitchToSend = self.targetYController.step(errorX, self.pollingTime)
-        #self.sendPitch(pitchToSend)
-        #self.sendRoll(rollToSend)
+        """ Send parameters to copter, our view of the roll is inverted
+            what it should be on the copter, so we change the roll sign """
         self.sendParametersToCopter(pitchToSend, -rollToSend, 0)
         #Save in list constantly
-        self.calibrationParameters = [pitchToSend, rollToSend] 
+        self.calibrationParameters = [pitchToSend, -rollToSend] 
         
     def isInDeadZone(self):
+        """ Method to see if copter is outside of the safe zone to fly
+        
+            Returns
+            -------
+            boolean value
+                True if copter is in deadZone, false if its not
+        
+        """
         if ((self.copterXPos > self.maxX) or (self.copterXPos < self.minX) 
             or (self.copterYPos > self.maxY) or(self.copterYPos < self.minY)):
-            self.killCopter();
             return True
         return False
 
@@ -189,9 +259,8 @@ myCalibrator.setBasePosition(750,600)
 myCalibrator.setPollingTime(0.005) #optimum: 0.005
 myCalibrator.setAircraftID(5)
 myCalibrator.myIvyCalNode.IvyInitStart()
-myCalibrator.myIvyCalNode.IvySendCalParams(myCalibrator.aircraftID, 0, 0, 0, 0)
+myCalibrator.sendParametersToCopter(0, 0, 0) #We make sure pitch, roll and yaw are 0 at start
 myCalibrator.unkillCopter()
-myCalibrator.sendParametersToCopter(0, 0, 0)
 time.sleep(3) #For the camera to detect the initial position
 myCalibrator.sendStartMode() #I uncommented this for simulation purposes
 time.sleep(1.75) #When the copter turns on, there are no lights until a few seconds
@@ -214,7 +283,3 @@ time.sleep(2)
 myCalibrator.killCopter()
 print("ProgramEnded")
 raise SystemExit()
-
-
-
-
