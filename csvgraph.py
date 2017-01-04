cf92eac88a5f57473edd28dee69750ad4d154f88
finkeneers <finkeneers@iws.cs.ovgu.de>
2017-01-04 15:07:18 +0100

    put graphs in a folder for documentation purposes

--- csvgraph.py
+++ csvgraph.py
@@ -2,12 +2,21 @@ from csv import reader
 from matplotlib import pyplot
 import math
 
-with open('./csvOutput/RefeedRun7/2017-01-03_03-37-59.csv', 'r') as f:
+
+"""open file:
+./csvOutput/RefeedRunX for refeed runs
+./csvOutput/FreshRuns/ for fresh runs
+"""
+with open('./quickcsv/refeed7-1.csv', 'r') as f:
     data = list(reader(f))
 
+"""select columns, 23/24 for pitch/roll, 9/10 for errorx/"""
+
+
 time = [i[1] for i in data[1::]]
-errorx = [i[23] for i in data[1::]]
-errory = [i[24] for i in data[1::]]
+errorx = [i[9] for i in data[1::]]  
+errory = [i[10] for i in data[1::]]
+"""calculate average error values, comment out for calib values"""
 
 absX = [abs(float(x)) for x in errorx]
 AvgX = 0.1*(math.floor(sum(absX)/len(absX)*10))
@@ -16,12 +25,23 @@ AvgX = str(AvgX)
 absY = [abs(float(y)) for y in errory]
 AvgY = 0.1*(math.floor(sum(absY)/len(absY)*10))
 AvgY = str(AvgY)
-
-pyplot.plot(time, errorx, label='Pitch')
-pyplot.plot(time, errory, label='Roll')
-pyplot.legend()
 pyplot.figtext(0.5,0.8, "AvgX="+AvgX+"\nAvgY="+AvgY)
-pyplot.title('Error over Time')
+
+
+"""final calib values, comment out for error values"""
+"""
+finalPitch=errorx[-1]
+finalRoll=errory[-1]
+finalPitch=finalPitch[:7]
+finalRoll=finalRoll[:7]
+pyplot.figtext(0.5,0.8, "Pitch="+str(finalPitch)+"\nRoll="+str(finalRoll))
+"""
+
+pyplot.plot(time, errorx, label='errorx')
+pyplot.plot(time, errory, label='errory')
+pyplot.legend()
+
+pyplot.title('error over Time')
 pyplot.xlabel('Time/seconds')
-pyplot.savefig('Calibration values over Time RefeedRun 7.png')
+pyplot.savefig('./quickcsv/error values over Time refeed7-1.png')
 pyplot.show()
